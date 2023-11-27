34591f8f89c1c842c7c9026b28b0b92b7d84bfb6
TOMAZO77 <xemenone@hotmail.com>
2023-11-27 12:38:59 +0000

    Add project files.

---
+++ area of rectangle/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace area_of_rectangle
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.areatotal = new System.Windows.Forms.TextBox();
+            this.width = new System.Windows.Forms.TextBox();
+            this.length = new System.Windows.Forms.TextBox();
+            this.height = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(399, 89);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(286, 143);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(76, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "AREA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // areatotal
+            // 
+            this.areatotal.Location = new System.Drawing.Point(62, 138);
+            this.areatotal.Multiline = true;
+            this.areatotal.Name = "areatotal";
+            this.areatotal.Size = new System.Drawing.Size(175, 46);
+            this.areatotal.TabIndex = 2;
+            // 
+            // width
+            // 
+            this.width.Location = new System.Drawing.Point(502, 238);
+            this.width.Name = "width";
+            this.width.Size = new System.Drawing.Size(70, 27);
+            this.width.TabIndex = 3;
+            // 
+            // length
+            // 
+            this.length.Location = new System.Drawing.Point(323, 138);
+            this.length.Name = "length";
+            this.length.Size = new System.Drawing.Size(70, 27);
+            this.length.TabIndex = 4;
+            this.length.TextChanged += new System.EventHandler(this.length_TextChanged);
+            // 
+            // height
+            // 
+            this.height.Location = new System.Drawing.Point(602, 138);
+            this.height.Name = "height";
+            this.height.Size = new System.Drawing.Size(70, 27);
+            this.height.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(332, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "length";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(512, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "width";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(612, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "height";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(76, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 52);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "VOLUME";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.height);
+            this.Controls.Add(this.length);
+            this.Controls.Add(this.width);
+            this.Controls.Add(this.areatotal);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox areatotal;
+        private System.Windows.Forms.TextBox width;
+        private System.Windows.Forms.TextBox length;
+        private System.Windows.Forms.TextBox height;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
